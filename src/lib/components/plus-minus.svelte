2c47de8c3459593319eaff4065602212366e6070
mr-lemoncello <vikasarino@gmail.com>
2026-01-21 23:08:31 -0800

    plus minus button

---
+++ src/lib/components/plus-minus.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	interface Props {
+		value: number;
+		count?: number;
+		disabled?: boolean;
+	}
+
+	let { value, count = $bindable<number>(), disabled = false }: Props = $props();
+
+	function handleClick() {
+		count += value;
+	}
+</script>
+
+<button
+	class="m-2.5 inline-flex justify-center rounded-lg border-5
+	       border-[#e7c600] bg-[#5faad7] px-8 py-2
+	       transition-transform duration-300 hover:scale-110"
+	onclick={handleClick}
+	{disabled}
+>
+	{value > 0 ? `+${value}` : value}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import PlusMinus from '$lib/components/plus-minus.svelte';
+    let count = $state(0);
+</script>
+
+<div
+	class="m-3 inline-flex flex-row flex-wrap gap-1
+	       rounded-lg border-5 border-[#1b2d48]
+	       bg-[#1f3454] p-1">
+<PlusMinus value={-1} bind:count />
+<PlusMinus value={+1} bind:count />
+<PlusMinus value={+5} bind:count />
+</div>
+
+<p class="mx-3 mt-2 font-mono text-lg">Counter: {count}</p>
