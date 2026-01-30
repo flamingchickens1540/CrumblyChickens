cbfc30442e6499b1f6537bbe07963d023605cbb6
autumnwilkes <autumnawilkes@gmail.com>
2026-01-30 15:34:51 -0800

    feat(wip): start of schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,3 +1,50 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import {
+	pgTable as table,
+	serial,
+	integer,
+	text,
+	boolean,
+	pgEnum,
+	varchar,
+	unique,
+	primaryKey
+} from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', { id: serial('id').primaryKey(), age: integer('age') });
+export const user = table('user', { id: serial('id').primaryKey(), age: integer('age') });
+
+export const teamEvent = table('team_event', {
+	teamKey: integer('team_key'),
+	eventKey: varchar('event_key', { length: 64 }).references(() => event.eventKey)
+});
+export const event = table('event', {
+	eventKey: varchar('event_key', { length: 64 })
+});
+
+export const endgame = pgEnum('endgame', ['L1', 'L2', 'L3', 'None']);
+export const teamMatch = table(
+	'team_match',
+	{
+		teamKey: integer('team_key'),
+		matchKey: varchar('match_key', { length: 64 }),
+		eventKey: varchar('event_key', { length: 64 }),
+		autoStart: varchar('auto_start_location', { length: 16 }),
+		autoClimb: boolean('auto_climb'),
+		autoHub: integer('auto_hub'),
+		autoShuffle: integer('auto_shuffle'),
+		teleHub: integer('tele_hub'),
+		teleShuffle: integer('tele_shuffle'),
+		teleSteal: integer('tele_steal'),
+		driverSkill: integer('driver_skill'),
+		endgame: endgame(),
+		died: boolean('died'),
+		notes: text('notes')
+	},
+	(table) => {
+		return {
+			teamMatchKeyUnique: primaryKey({
+				name: 'team_match_key_unique',
+				columns: [table.teamKey, table.matchKey]
+			})
+		};
+	}
+);
