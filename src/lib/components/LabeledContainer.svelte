34a59fea5f5b0fcb4e528f7959e47348e5245a17
Hanya You <youh@catlin.edu>
2026-02-04 10:53:18 -0800

    pitscout collect frontend mostly done, styling still needs fixing

--- src/lib/components/LabeledContainer.svelte
+++ src/lib/components/LabeledContainer.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 
-    const { label, isYellow = false }: {label: string, isYellow: boolean} = $props()
+    const { label, isYellow = false, padded = false }: {label: string, isYellow: boolean, padded: boolean} = $props()
 </script>
 
-<p class="text-3xl text-slate-400 mt-2 mb-1 text-left mx-2.5">
+<p class="text-3xl text-slate-400 mt-3 mb-2 text-left mx-2.5">
     {label}
 </p>
-<div class="rounded justify-self-start justify-self-center mx-2.5 {isYellow ? 'bg-amber-300 text-black' : 'bg-slate-900 text-white'} text-center p-3 text-xl">
+<div class="rounded justify-self-start justify-self-center mx-2.5 {isYellow ? 'bg-amber-300 text-black' : 'bg-slate-900 text-white'} {padded ? 'p-3' : 'p-0 bg-transparent'} text-center text-xl">
     <slot/>
 </div>
