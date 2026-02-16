c3004c2b4d5ac33b83ce8d0faa0bec26c0f5d97a
Azalea Colburn <62953415+azaleacolburn@users.noreply.github.com>
2026-02-15 21:06:51 -0800

    chore: dev shell + remove docker (#23)

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        with nixpkgs.lib;
+        f: genAttrs systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              postgresql_17
+              bun
+              nodejs
+              nodePackages.prettier
+              openssl
+            ];
+            shellHook = ''
+              export PG=$PWD/.dev_postgres/
+              export PGDATA="$PG"data
+              export PGPORT=5432
+              export PGHOST=localhost
+              export PGUSER=$USER
+              export PGPASSWORD=postgres
+              export PGDATABASE=example
+              export DATABASE_URL=postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE
+
+              pg_setup() {
+                pg_stop;
+                rm -rf $PG;
+                initdb -D $PGDATA &&
+                echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf &&
+                pg_ctl -D $PGDATA -l $PG/postgres.log start &&
+                createdb
+              }
+
+              alias pg_start="pg_ctl -D $PGDATA -l $PG/postgres.log start"
+              alias pg_stop="pg_ctl -D $PGDATA stop"
+              alias pg_drop="psql -U $PGUSER -c 'DROP DATABASE $PGDATABASE'"
+              alias exit="(! pidof postgres || pg_ctl -D $PGDATA stop) && exit"
+            '';
+          };
+      });
+    };
+}
