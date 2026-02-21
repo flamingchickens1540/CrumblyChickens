64f99fa0094ca0b3474cbc9fd986e34a55c27581
autumnwilkes <autumnawilkes@gmail.com>
2026-02-20 19:49:28 -0800

    fix: remove empty files

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 export const actions = {
     login: async ({ cookies, request }) => {
         const data = await request.formData();
-        const username = data.get('user');
+        const username = data.get('username');
 
         if (!username) {
             return;
