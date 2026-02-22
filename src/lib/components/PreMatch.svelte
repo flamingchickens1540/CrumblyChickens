ee326dad83a184e632013f357f24da025c21d4e2
autumnwilkes <autumnawilkes@gmail.com>
2026-02-22 08:58:16 -0800

    fix: prematch binding

--- src/lib/components/PreMatch.svelte
+++ src/lib/components/PreMatch.svelte
@@ -8,14 +8,19 @@
         matchData = $bindable(),
         stage = $bindable()
     }: { matchData: TeamMatch; stage: GameStage } = $props();
-
+    let fielded = $state('Feilded');
+    let autoStart = $state('Outpost');
+    $effect(() => {
+        matchData.fielded = fielded === 'Fielded';
+        matchData.autoStart = autoStart as 'Outpost' | 'Tower' | 'Depot';
+    });
     let fakePlusMinus: boolean = $state(false);
 </script>
 
 <div class="grid-wrap mx-3 mt-0 mb-3 grid auto-cols-fr px-1 pt-0 pb-1">
-    <VerticalToggleGroup items={['Outpost', 'Tower', 'Depot']} bind:value={matchData.autoStart} />
+    <VerticalToggleGroup items={['Outpost', 'Tower', 'Depot']} bind:value={autoStart} />
     <!-- TODO Bind selected values as booleans here -->
-    <HorizontalToggleGroup items={['Fielded', 'Missed Match']} />
+    <HorizontalToggleGroup bind:value={fielded} items={['Fielded', 'Missed Match']} />
 </div>
 
 <BottomButton {matchData} bind:stage bind:plusMinus={fakePlusMinus} />
