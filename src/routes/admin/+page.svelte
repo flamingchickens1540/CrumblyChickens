a07e58559561abe43e3b02b5020c9a2839de9364
autumnwilkes <autumnawilkes@gmail.com>
2026-02-21 20:39:54 -0800

    feat: maybe made tracking of matches better

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { io, type Socket } from 'socket.io-client';
-    import { type Match } from '$lib/types';
+    import { type Match, type Robot } from '$lib/types';
     import type { PageProps } from './$types';
     type NewMatch = {
         matchKey: string;
@@ -12,58 +12,32 @@
     const socket: Socket = io('/admin', { auth: { username: data.user } });
     let scouts: string[] = $state([]);
 
-    let event_key: string = $state("")
+    let event_key: string = $state('');
 
-    let currentMatch: Match = $state(emptyMatch());
+    let currentMatch: Match | null = $state(null);
     let nextMatch: NewMatch = $state(emptyNextMatch());
-    socket.on(
-        'handshake_data',
-        ([scoutQueue, robotQueue]: [string[], { teamKey: number; color: 'red' | 'blue' }[]]) => {
-            console.log(scouts);
-            scouts = scoutQueue;
-            currentMatch = emptyMatch();
-            let redI = 0;
-            let blueI = 0;
-            for (const robot of robotQueue) {
-                if (robot.color === 'red') {
-                    currentMatch.red[redI].teamKey = robot.teamKey;
-                    redI += 1;
-                } else {
-                    currentMatch.blue[blueI].teamKey = robot.teamKey;
-                    blueI += 1;
-                }
-            }
-        }
-    );
+
+    socket.on('handshake_data', ([scoutQueue, match]: [string[], Match | null]) => {
+        console.log('replaced scout queue: ' + $state.snapshot(scouts));
+        scouts = scoutQueue;
+        currentMatch = match;
+    });
     socket.on('scout_left_queue', (username) => {
-        const i = scouts.indexOf(username)
+        const i = scouts.indexOf(username);
         if (i == -1) return;
 
-        scouts.splice(i, 1)
-    })
+        scouts.splice(i, 1);
+    });
     socket.on('scout_joined_queue', (username) => scouts.push(username));
-    socket.on('scout_recieved_robot', (_robot, username) => {
+    socket.on('scout_recieved_robot', ([match, username]) => {
+        console.log(match);
+        currentMatch = match;
         scouts.splice(scouts.indexOf(username), 1);
     });
 
-    function emptyMatch(): Match {
-        return {
-            matchKey: '',
-            red: [
-                { status: 'Unassigned', teamKey: NaN },
-                { status: 'Unassigned', teamKey: NaN },
-                { status: 'Unassigned', teamKey: NaN }
-            ],
-            blue: [
-                { status: 'Unassigned', teamKey: NaN },
-                { status: 'Unassigned', teamKey: NaN },
-                { status: 'Unassigned', teamKey: NaN }
-            ]
-        };
-    }
     function clearRobots() {
         socket.emit('clear_robots');
-        currentMatch = emptyMatch();
+        currentMatch = null;
     }
     function removeScout(username: string) {
         scouts.splice(scouts.indexOf(username), 1);
@@ -72,51 +46,42 @@
     function sendMatch() {
         const parsedMatch = parseNextMatch();
         if (!parsedMatch) {
-            alert("Invalid Match Key")
             return;
         }
-        const key = parsedMatch.matchKey;
         nextMatch = emptyNextMatch();
 
-        const robotQueue = [
-            ...parsedMatch.red.map((robot) => {
-                return { teamKey: robot.teamKey, color: 'red' };
-            }),
-            ...parsedMatch.blue.map((robot) => {
-                return { teamKey: robot.teamKey, color: 'blue' };
-            })
-        ];
-        socket.emit('send_match', { teams: robotQueue, key });
+        socket.emit('send_match', parsedMatch);
         currentMatch = parsedMatch;
     }
     function emptyNextMatch(): NewMatch {
         return { matchKey: '', red: ['', '', ''], blue: ['', '', ''] };
     }
     function parseNextMatch(): Match | null {
+        const red = nextMatch.red.map((key) => {
+            return { status: 'Unassigned', teamKey: parseInt(key) };
+        }) satisfies Robot[];
+        const blue = nextMatch.blue.map((key) => {
+            return { status: 'Unassigned', teamKey: parseInt(key) };
+        }) satisfies Robot[];
+
+        for (let i = 0; i < 3; i++) {
+            if (
+                red[i] === undefined ||
+                blue[i] === undefined ||
+                !red[i].teamKey ||
+                !blue[i].teamKey
+            ) {
+                alert('Bad upcoming match');
+                console.error(nextMatch);
+                return null;
+            }
+        }
+
         const parsed = {
             matchKey: nextMatch.matchKey,
-            red: [
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.red[0]) },
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.red[1]) },
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.red[2]) }
-            ],
-
-            blue: [
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.blue[0]) },
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.blue[1]) },
-                { status: 'Unassigned', teamKey: parseInt(nextMatch.blue[2]) }
-            ]
+            red: [red[0], red[1], red[2]],
+            blue: [blue[0], blue[1], blue[2]]
         } satisfies Match;
-        parsed.red.forEach((robot) => {
-            if (!robot.teamKey) {
-                return null;
-            }
-        });
-        parsed.blue.forEach((robot) => {
-            if (!robot.teamKey) {
-                return null;
-            }
-        });
 
         return parsed;
     }
@@ -131,7 +96,7 @@
     const eventKey = '2026week0';
 
     // For api stuff
-    let _matchKey = $derived(eventKey + '_' + (currentMatch.matchKey ?? ''));
+    let _matchKey = $derived(eventKey + '_' + (currentMatch?.matchKey ?? ''));
 
     const getColor = (status: 'Pending' | 'Unassigned' | 'Submitted'): string => {
         switch (status) {
@@ -146,21 +111,21 @@
 
     /// Loads the teams from the next match into the admin page
     async function loadMatch() {
-        const res = await fetch(`/api/load/match?key=${nextMatch.matchKey}`)
+        const res = await fetch(`/api/load/match?key=${nextMatch.matchKey}`);
 
         // TODO Finish in the morning
     }
 
     /// Loads teams from an event to a DB
     async function loadTeamsToDB() {
-        await fetch("/api/load/event", {
-            method: "POST",
+        await fetch('/api/load/event', {
+            method: 'POST',
             body: event_key
-        })
+        });
     }
 </script>
 
-<div class="grid grid-cols-3 gap-2 text-white mt-2 mx-2">
+<div class="mx-2 mt-2 grid grid-cols-3 gap-2 text-white">
     <div class="flex flex-col gap-2">
         <div class="bg-gunmetal flex flex-col gap-2 rounded p-2">
             <div class="grid grid-cols-2 gap-4">
@@ -173,10 +138,18 @@
             </div>
             <div class="rounded-2 grid grid-cols-3 gap-2">
                 {#each nextMatch.red as _, i (i)}
-                    <input class="bg-first-red h-12 rounded p-2" bind:value={nextMatch.red[i]} placeholder="Red{i + 1}"/>
+                    <input
+                        class="bg-first-red h-12 rounded p-2"
+                        bind:value={nextMatch.red[i]}
+                        placeholder="Red{i + 1}"
+                    />
                 {/each}
                 {#each nextMatch.blue as _, i (i)}
-                    <input class="bg-first-blue h-12 rounded p-2" bind:value={nextMatch.blue[i]} placeholder="Blue{i + 1}"/>
+                    <input
+                        class="bg-first-blue h-12 rounded p-2"
+                        bind:value={nextMatch.blue[i]}
+                        placeholder="Blue{i + 1}"
+                    />
                 {/each}
             </div>
         </div>
@@ -187,24 +160,41 @@
                 <span class="justify-self-center">Current Match</span>
             </div>
             <div class="grid max-h-28 grid-cols-3 gap-2">
-                {#each currentMatch.red as tm, i (i)}
-                    <button
-                        class="{getColor(
-                            tm.status
-                        )} grid h-12 grid-cols-2 place-items-center rounded p-2"
-                        >{filter(tm.teamKey)}
-                        <div class="bg-first-red size-6 rounded-full"></div>
-                    </button>
-                {/each}
-                {#each currentMatch.blue as tm, i (i)}
-                    <button
-                        class="{getColor(
-                            tm.status
-                        )} grid h-12 grid-cols-2 place-items-center rounded p-2"
-                        >{filter(tm.teamKey)}
-                        <div class="bg-first-blue size-6 rounded-full"></div>
-                    </button>
-                {/each}
+                {#if currentMatch === null}
+                    {#each Array(3) as _, i (i)}
+                        <div class="grid grid-rows-2 gap-2">
+                            <button
+                                class="bg-eerie-black grid h-12 grid-cols-2 place-items-center rounded p-2"
+                                >-
+                                <div class="bg-first-red size-6 rounded-full"></div>
+                            </button>
+                            <button
+                                class="bg-eerie-black grid h-12 grid-cols-2 place-items-center rounded p-2"
+                                >-
+                                <div class="bg-first-blue size-6 rounded-full"></div>
+                            </button>
+                        </div>
+                    {/each}
+                {:else}
+                    {#each currentMatch.red as tm, i (i)}
+                        <button
+                            class="{getColor(
+                                tm.status
+                            )} grid h-12 grid-cols-2 place-items-center rounded p-2"
+                            >{filter(tm.teamKey)}
+                            <div class="bg-first-red size-6 rounded-full"></div>
+                        </button>
+                    {/each}
+                    {#each currentMatch.blue as tm, i (i)}
+                        <button
+                            class="{getColor(
+                                tm.status
+                            )} grid h-12 grid-cols-2 place-items-center rounded p-2"
+                            >{filter(tm.teamKey)}
+                            <div class="bg-first-blue size-6 rounded-full"></div>
+                        </button>
+                    {/each}
+                {/if}
             </div>
         </div>
     </div>
@@ -220,11 +210,18 @@
                 {/each}
             </div>
         </div>
-        <div class="bg-gunmetal flex flex-col gap-2 p-2 rounded">
+        <div class="bg-gunmetal flex flex-col gap-2 rounded p-2">
             <button onclick={clearRobots} class="bg-eerie-black rounded p-2">Clear Robots</button>
-            <hr class="text-gray-500"/>
-            <input class="bg-eerie-black rounded p-2" type="text" bind:value={event_key} placeholder="Event Key">
-            <button class="bg-eerie-black p-2 rounded" onclick={loadTeamsToDB}>Load New Event</button>
+            <hr class="text-gray-500" />
+            <input
+                class="bg-eerie-black rounded p-2"
+                type="text"
+                bind:value={event_key}
+                placeholder="Event Key"
+            />
+            <button class="bg-eerie-black rounded p-2" onclick={loadTeamsToDB}
+                >Load New Event</button
+            >
         </div>
     </div>
 </div>
--- ws.ts
+++ ws.ts
@@ -1,140 +1,160 @@
-import { type ViteDevServer } from "vite";
-import { Server } from "socket.io";
-import { spawn } from "child_process";
+import { type ViteDevServer } from 'vite';
+import { Server } from 'socket.io';
+import { spawn } from 'child_process';
+import type { Match } from '@/types';
 
 const info = (s: string) => console.log(`\x1b[32m${s}\x1b[0m`);
 const warn = (s: string) => console.log(`\x1b[33m${s}\x1b[0m`);
 
-let match_key = "";
-let robotQueue: { teamKey: number; color: "red" | "blue" }[] = [];
+let currentMatch: Match | null;
+let matchIdx = 0;
 
 const wsServer = {
-    name: "wsServer",
+    name: 'wsServer',
     configureServer(server: ViteDevServer) {
         if (!server.httpServer) return;
         const io = new Server(server.httpServer);
-        io.on("connect", (socket) => {
+        io.on('connect', (socket) => {
             if (!socket.handshake.auth.username) {
-                warn(
-                    `User joined without username. Disconnecting. Socket id: ${socket.id}`,
-                );
+                warn(`User joined without username. Disconnecting. Socket id: ${socket.id}`);
 
                 socket.disconnect();
                 return;
             }
         });
-        io.of("/queue").on("connect", (socket) => {
-            const robot = robotQueue.pop();
+        io.of('/queue').on('connect', (socket) => {
+            let robot = getNextTeam(socket.handshake.auth.username);
             if (robot !== undefined) {
-                info(
-                    `${socket.handshake.auth.username} recieved robot ${robot?.teamKey}`,
-                );
-                socket.emit("recieve_robot", { robot, match_key });
+                info(`${socket.handshake.auth.username} recieved robot ${robot.teamKey}`);
+                socket.emit('recieve_robot', { robot, matchKey: currentMatch!.matchKey });
                 socket.disconnect();
-                io.of("/admin").emit("scout_recieved_robot", [
-                    robot,
-                    socket.handshake.auth.username,
+                io.of('/admin').emit('scout_recieved_robot', [
+                    currentMatch,
+                    socket.handshake.auth.username
                 ]);
             } else {
-                io.of("/admin").emit(
-                    "scout_joined_queue",
-                    socket.handshake.auth.username,
-                );
+                io.of('/admin').emit('scout_joined_queue');
                 info(`${socket.handshake.auth.username} joined scout queue`);
             }
 
-            socket.on("leave_queue", () => {
+            socket.on('leave_queue', () => {
                 socket.disconnect();
             });
-            socket.on("disconnect", async (_) => {
+            socket.on('disconnect', async (_) => {
                 info(`${socket.handshake.auth.username} left queue`);
-                io.to("admin").emit(
-                    "scout_left_queue",
-                    socket.handshake.auth.username,
-                );
+                io.of('admin').emit('scout_left_queue', socket.handshake.auth.username);
             });
         });
-        io.of("/admin").on("connect", (socket) => {
-            const scoutQueue = [
-                ...io
-                    .of("/queue")
-                    .sockets.values()
-                    .map((scout) => scout.handshake.auth.username),
-            ];
+        io.of('/admin').on('connect', (socket) => {
+            const scoutQueue: string[] = io
+                .of('/queue')
+                .sockets.values()
+                .map((scout) => scout.handshake.auth.username)
+                .toArray();
             info(`Admin aquired: ${socket.handshake.auth.username}`);
 
-            socket.emit("handshake_data", [scoutQueue, robotQueue]);
+            socket.emit('handshake_data', [scoutQueue, currentMatch]);
 
-            socket.on("clear_robots", () => {
-                info(`Robot queue cleared. Was ${robotQueue}`);
-                robotQueue = [];
+            socket.on('clear_robots', () => {
+                info(`Current match cleared ${formatMatch()}`);
+                currentMatch = null;
             });
-            socket.on("remove_scout", (username: string) => {
-                const scouts = io.of("/queue").sockets.values();
+            socket.on('remove_scout', (username: string) => {
+                const scouts = io.of('/queue').sockets.values();
                 for (const scout of scouts) {
                     if (username === scout.handshake.auth.username) {
-                        scout.emit("leave_queue");
+                        // scout.disconnect();
+                        scout.emit('leave_queue');
                         info(`${username} removed from queue by admin`);
                     }
                 }
-                warn(
-                    `Attempted to remove a scout who wasn't in the queue: ${username}`,
-                );
+                warn(`Attempted to remove a scout who wasn't in the queue: ${username}`);
             });
-            socket.on(
-                "send_match",
-                (match: {
-                    teams: { teamKey: number; color: "red" | "blue" }[];
-                    key: string;
-                }) => {
-                    console.log(match.teams);
-                    const script = spawn("python3", ["export/data_export.py"]);
+            socket.on('send_match', (match: Match) => {
+                /* const script = spawn('python3', ['export/data_export.py']);
 
-                    script.stdout.on("data", (data) => {
-                        console.log(`Exported: ${data}`);
-                    });
+                script.stdout.on('data', (data) => {
+                    console.log(`Exported: ${data}`);
+                });
 
-                    script.stderr.on("data", (data) => {
-                        console.error(`Error Exporting: ${data}`);
-                    });
+                script.stderr.on('data', (data) => {
+                    console.error(`Error Exporting: ${data}`);
+                });*/
+                matchIdx = 0;
+                currentMatch = match;
+                const scouts = io.of('/queue');
+                info(`New Match ${formatMatch()}`);
 
-                    match_key = match.key;
-                    robotQueue = match.teams;
-
-                    const scouts = io.of("/queue");
-                    const formattedTeams: string = match.teams
-                        .map((team) => {
-                            if (team.color == "red") {
-                                return ` \x1b[31m${team.teamKey}\x1b[0m`;
-                            } else {
-                                return ` \x1b[34m${team.teamKey}\x1b[0m`;
-                            }
-                        })
-                        .join();
-                    info(`New Match:${formattedTeams}`);
-
-                    for (const scout of scouts.sockets.values()) {
-                        const robot = robotQueue.pop();
-                        if (robot === undefined) {
-                            break;
-                        }
-                        scout.emit("recieve_robot", {
-                            robot,
-                            match_key,
-                        });
-                        io.of("/admin").emit("scout_recieved_robot", [
-                            robot,
-                            scout.handshake.auth.username,
-                        ]);
-                        info(
-                            `${scout.handshake.auth.username} recieved robot ${robot.teamKey} from queue`,
-                        );
-                        scout.disconnect();
+                for (const scout of scouts.sockets.values()) {
+                    const robot = getNextTeam(scout.handshake.auth.username);
+                    if (robot === undefined) {
+                        break;
                     }
-                },
-            );
+                    scout.emit('recieve_robot', {
+                        robot,
+                        matchKey: match.matchKey
+                    });
+                    io.of('/admin').emit('scout_recieved_robot', [
+                        match,
+                        scout.handshake.auth.username
+                    ]);
+                    info(
+                        `${scout.handshake.auth.username} recieved robot ${robot.teamKey} from queue`
+                    );
+                    scout.disconnect();
+                }
+                io.of('/admin').emit('handshake_data', [
+                    io
+                        .of('/queue')
+                        .sockets.values()
+                        .map((scout) => scout.handshake.auth.username)
+                        .toArray(),
+                    currentMatch
+                ]);
+            });
         });
-    },
+    }
 };
 
+function getNextTeam(scout: string): { teamKey: number; color: 'red' | 'blue' } | undefined {
+    let teamKey;
+    if (currentMatch === null) {
+        return undefined;
+    }
+    let color: 'red' | 'blue' | undefined;
+    if (matchIdx < 3) {
+        teamKey = currentMatch.red[matchIdx].teamKey;
+        currentMatch.red[matchIdx] = {
+            status: 'Pending',
+            teamKey,
+            scout
+        };
+        color = 'red';
+    } else if (matchIdx < 6) {
+        teamKey = currentMatch.red[matchIdx - 3].teamKey;
+        currentMatch.red[matchIdx - 3] = {
+            status: 'Pending',
+            teamKey,
+            scout
+        };
+        color = 'blue';
+    }
+    matchIdx++;
+    if (teamKey !== undefined && color !== undefined) {
+        return { teamKey, color };
+    }
+}
+
+function formatMatch(): string {
+    if (currentMatch === null) {
+        return 'match previously cleared';
+    }
+    return (
+        currentMatch.matchKey +
+        ': ' +
+        currentMatch.red.map((red) => ` \x1b[31m${red.teamKey}\x1b[0m`).join() +
+        '\n' +
+        currentMatch.blue.map((blue) => ` \x1b[34m${blue.teamKey}\x1b[0m`).join()
+    );
+}
 export default wsServer;
