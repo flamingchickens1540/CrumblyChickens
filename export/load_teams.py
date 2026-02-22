a3b4958534cff289a2a2f5493584ee5afe57cb1b
Azalea Colburn <azaleacolburn@gmail.com>
2026-02-22 11:04:50 -0800

    feat: load teams script

---
+++ export/load_teams.py
@@ -0,0 +1,40 @@
+import os
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Connect to database
+test = os.environ['DATABASE_URL']
+password = os.environ['PGPASSWORD']
+user = os.environ['PGUSER']
+dbname = os.environ['PGDATABASE']
+port = os.environ['PGPORT']
+host = os.environ['PGHOST']
+
+conn = psycopg.connect(
+    dbname=dbname,
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
+cur = conn.cursor()
+
+teams = [
+    "1540",
+    "1359",
+    "997",
+    "955",
+    "971",
+    "2521",
+    "2898",
+    "2374",
+    "2046",
+]
+
+for team in teams:
+    cur.execute("INSERT INTO team (teamKey, name) VALUES: (%s, %s) ON CONFLICT DO NOTHING;", (team, ""))
+
+cur.close()
+conn.close()
---
+++ src/lib/components/DoubleButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let {
+
+    }: { matchData: TeamMatch; stage: GameStage; plusMinus: boolean } = $props();
+
+    const bottomBtnClass = `m-2.5 inline-flex items-center justify-center
+		rounded-md px-8 py-2 drop-shadow-xl transition-transform duration-300 hover:scale-105
+		bottom-3 left-3 right-3 p-2 bg-[#5C5C5C] hover:bg-[#7D7D7D]`;
+</script>
