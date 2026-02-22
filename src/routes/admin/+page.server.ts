ac4706eefe17306cb52bb5b8ff9d5f5a985bfe73
autumnwilkes <autumnawilkes@gmail.com>
2026-02-21 19:36:39 -0800

    fix: layout applying to login caused infinite loop

---
+++ src/routes/admin/+page.server.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ cookies }) => {
+    const user = cookies.get('user');
+
+    if (!user) {
+        throw redirect(303, '/login');
+    }
+
+    return {
+        user
+    };
+};
