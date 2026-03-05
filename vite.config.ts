0aea4fef4fcbaf970907f84dd880a350a906f033
autumnwilkes <autumnawilkes@gmail.com>
2026-03-04 23:29:55 -0800

    feat: made app runnable in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import wsServer from './ws.ts';
+import wsConfig from './ws.ts';
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit(), wsServer],
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        {
+            name: 'wsServer',
+            configureServer(server) {
+                if (!server.httpServer) return;
+                wsConfig(server.httpServer);
+            }
+        }
+    ],
     server: {
         allowedHosts: true
     }
