7cc289358352ea9ab688bc383c8e01c3c9608884
Hanya You <youh@catlin.edu>
2026-03-14 18:21:02 -0700

    feat: size of name on LB is dependent on how many matches you scouted. slightly cursed.

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -3,10 +3,10 @@
 
     let users: Array<{name: string; matches: number}> = [
         {name: "SampleUser", matches: 1540},
-        {name: "SampleUser2", matches: 4},
-        {name: "SampleUser3", matches: 3},
-        {name: "SampleUser4", matches: 2},
-        {name: "SampleUser5", matches: 0}
+        {name: "SampleUser2", matches: 957},
+        {name: "SampleUser3", matches: 254},
+        {name: "SampleUser4", matches: 118},
+        {name: "SampleUser5", matches: 4}
     ]
     let totalMatches: number = users.reduce((total, data) => {
         return total+data.matches;
@@ -46,11 +46,13 @@
             </span> as many matches as you!
         {/if}
     </div>
-    <div class="mx-2.5 rounded bg-[#2c2c2c] text-white text-center text-xl mt-2">
+    <div class="mx-2.5 rounded bg-[#2c2c2c] text-white text-center mt-2">
         {#each users as user, i}
             <button onclick={you == i ? () => {} : () => selectedUser = i} class="w-full {(i == users.length-1) ? '' : 'border-b border-neutral-400'} {you == i ? 'bg-linear-to-r from-neutral-600 to-transparent' : ''} p-1 text-left ">
-                <span class="text-amber-300">#{i+1}</span>
-                <span class="{i < 3 ? 'bg-linear-to-r from-amber-400 to-red-400 bg-clip-text text-transparent':''}">{user.name}</span>
+                <span style="font-size: {14+user.matches*20/totalMatches}px">
+                    <span class="text-amber-300">#{i+1}</span>
+                    <span class="{i < 3 ? 'bg-linear-to-r from-amber-400 to-red-400 bg-clip-text text-transparent':''}">{user.name}</span>
+                </span>
                 <br>
                 <span class="text-sm italic text-neutral-400">
                     <span class="text-white">
